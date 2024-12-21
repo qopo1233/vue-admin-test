03f9fd560993c9be7e8dac45f7b2f514b0979fe1
yangchuanhua <yangchuan_hua@163.com>
2024-12-21 14:51:33 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang='ts'>
+
+</script>
+
+<template>
+  <div>
+    <h1>我是 App</h1>
+  </div>
+</template>
+
+<style scoped lang='scss'>
+
+</style>
---
+++ src/components/Person.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <el-button type="primary">点击</el-button>
+</template>
+
+<script setup lang="ts" name="person">
+
+</script>
+<style scoped>
+
+</style>
